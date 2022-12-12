02e81b783cc7c5064a2668e413134da89332e146
forcesumetee <46419997+forcesumetee@users.noreply.github.com>
2022-12-12 22:40:08 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class Program
+{
+    static void BuyBook(Stack<string> smartShelf, ref int transaction)
+    {
+        string book = Console.ReadLine();
+        Queue<string> tmpShelf = new Queue<string>();
+        while(smartShelf.GetLength() > 0)
+        {
+            string currentBook = smartShelf.Pop();
+            transaction++;
+            if(currentBook == book)
+            {
+                break;
+            }
+            else
+            {
+                tmpShelf.Push(currentBook);
+            }
+        }
+
+        while(tmpShelf.GetLength() > 0)
+        {
+            smartShelf.Push(tmpShelf.Pop());
+            transaction++;
+        }
+    }
+
+    static void InputBook(Stack<string> smartShelf)
+    {
+        string book;
+        while(true)
+        {
+            book = Console.ReadLine();
+            if(book == "Stop")
+            {
+                break;
+            }
+            smartShelf.Push(book);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Stack<string> smartShelf = new Stack<string>();
+        InputBook(smartShelf);
+
+        int transaction = 0;
+        while(smartShelf.GetLength() > 0)
+        {
+            BuyBook(smartShelf, ref transaction);
+        }
+
+        Console.WriteLine(transaction);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# mdt211_2565_week13_exercise3
+The 3rd exercise for the 13th week of MDT 211 (1/2565) class.
